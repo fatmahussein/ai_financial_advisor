e6e7d2a23935c0acfd8da53813cb8efd93b2a804
fatmahussein <fartunhussein346@gmail.com>
2025-07-11 22:52:01 +0300

    Add sync_emails action to fetch and store Gmail messages for current user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
+
+  def sync_emails
+  GmailService.new(current_user).fetch_and_store_messages
+  redirect_to home_index_path, notice: 'Emails synced!'
+end
+
 end
