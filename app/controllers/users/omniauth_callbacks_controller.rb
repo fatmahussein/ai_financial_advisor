8fd91dce85a0d484b494e405887b72a70e190b07
fatmahussein <fartunhussein346@gmail.com>
2025-07-11 21:35:46 +0300

    Add Google OAuth error handling and fix user persistence with random password

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :google_oauth2
+  
   def google_oauth2
-    @user = User.from_google(request.env['omniauth.auth'])
+    auth = request.env['omniauth.auth']
+
+    if auth.nil?
+      Rails.logger.error "Omniauth auth is NIL!"
+      return redirect_to root_path, alert: "Google login failed. Please try again."
+    end
+
+    @user = User.from_google(auth)
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      flash[:notice] = "Successfully signed in with Google"
+      flash[:notice] = "Signed in!"
     else
-      redirect_to root_path, alert: "Something went wrong"
+      redirect_to root_path, alert: "Something went wrong."
     end
   end
+
 end
