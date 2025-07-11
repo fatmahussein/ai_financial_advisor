1a0665bb39bb174341bba20468959a21b97385f2
fatmahussein <fartunhussein346@gmail.com>
2025-07-11 22:28:45 +0300

    Update the schema to include emails table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_11_140932) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_11_192512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "emails", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "gmail_id"
+    t.string "subject"
+    t.string "sender"
+    t.text "snippet"
+    t.text "body"
+    t.datetime "received_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_emails_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_11_140932) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "emails", "users"
 end
