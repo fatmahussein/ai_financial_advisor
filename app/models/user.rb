7c4d7247bcc2e9a928da60a135d9ea4cc77376e8
fatmahussein <fartunhussein346@gmail.com>
2025-07-11 17:33:04 +0300

    add Google OAuth integration to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: [:google_oauth2]
+
+    def self.from_google(auth)
+      user = where(email: auth.info.email).first_or_initialize
+      user.update(
+        name: auth.info.name,
+        provider: auth.provider,
+        uid: auth.uid,
+        google_token: auth.credentials.token,
+        google_refresh_token: auth.credentials.refresh_token,
+        token_expires_at: Time.at(auth.credentials.expires_at)
+      )
+      user
+    end
+
 end
