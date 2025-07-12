99d29387c01a7c17abaeecd603657b3977c946ca
fatmahussein <fartunhussein346@gmail.com>
2025-07-12 10:50:34 +0300

    enable pgvector extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_065847) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_074718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "vector"
 
   create_table "contacts", force: :cascade do |t|
     t.string "hubspot_id"
