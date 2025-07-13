b9f9713ed8e1255ad25c9f7c885089ec6c7507db
fatmahussein <fartunhussein346@gmail.com>
2025-07-13 12:07:30 +0300

    add SummarizationService to condense client communications using Ollama

---
+++ app/services/SummarizationService.rb
@@ -0,0 +1,16 @@
+# app/services/summarization_service.rb
+class SummarizationService
+  def initialize(user)
+    @ollama = OllamaService.new(user)
+  end
+
+  def summarize(text)
+    prompt = <<~PROMPT
+      Summarize the following client communication in one sentence:
+
+      #{text}
+    PROMPT
+
+    @ollama.answer(prompt)
+  end
+end
