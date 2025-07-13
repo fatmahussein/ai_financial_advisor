42c2f99653b3788b5868e2feef5e47b41627721a
fatmahussein <fartunhussein346@gmail.com>
2025-07-13 15:18:44 +0300

    Generate chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,10 +2,17 @@ class ChatsController < ApplicationController
   def index
   end
 
-  def history
+  def new
+    @chat = Chat.create!(title: "New Chat")
+    redirect_to chat_path(@chat)
   end
 
-  def new
+  def show
+    @chat = Chat.find(params[:id])
+  end
+
+   def history
+    @chats = Chat.order(created_at: :desc)
   end
 
   def create_message
